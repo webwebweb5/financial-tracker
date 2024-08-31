0ca0194d430146a098c2233fd8f4d020c39736ca
webwebweb5 <phiriyakornman@gmail.com>
2024-09-01 02:57:27 +0700

    feat: create auth page and import custom font

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,22 @@
-import { Button } from "./components/ui/button";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import DashboardPage from "./pages/dashboard";
+import AuthPage from "./pages/auth";
 
 function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <Button>Click me</Button>
-    </div>
+    <Router>
+      <main className="flex items-center justify-center w-full min-h-screen">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <DashboardPage />
+            }
+          />
+          <Route path="/auth" element={<AuthPage />} />
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
---
+++ client/src/pages/auth/index.tsx
@@ -0,0 +1,33 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+} from "@clerk/clerk-react";
+import { Navigate } from "react-router-dom";
+import { Button } from "../../components/ui/button";
+
+export default function AuthPage() {
+  return (
+    <main>
+      <SignedOut>
+        <h1 className="text-2xl font-bold">
+          Manage Your Own Personal Finance Tracker!
+        </h1>
+
+        <div className="flex gap-x-2 justify-center mt-4">
+          <Button asChild>
+            <SignUpButton mode="modal">Sign Up</SignUpButton>
+          </Button>
+
+          <Button asChild>
+            <SignInButton mode="modal">Sign In</SignInButton>
+          </Button>
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Navigate to="/" />
+      </SignedIn>
+    </main>
+  );
+}
